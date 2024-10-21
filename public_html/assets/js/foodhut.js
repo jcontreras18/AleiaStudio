cdfc5207a9641e33bf32874a2d13baebd987b0d2
eocampo9125 <36829968+eocampo9125@users.noreply.github.com>
2024-10-21 15:51:15 -0500

    update
    update

--- public_html/assets/js/foodhut.js
+++ public_html/assets/js/foodhut.js
@@ -13,100 +13,100 @@
 */
 
 // smooth scroll
-$(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
+$(document).ready(function () {
+  $(".navbar .nav-link").on('click', function (event) {
 
-        if (this.hash !== "") {
+    if (this.hash !== "") {
 
-            event.preventDefault();
+      event.preventDefault();
 
-            var hash = this.hash;
+      var hash = this.hash;
 
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 700, function(){
-                window.location.hash = hash;
-            });
-        } 
-    });
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 700, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
 });
 
 new WOW().init();
 
 function initMap() {
-    var uluru = {lat: 37.227837, lng: -95.700513};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 8,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
- }
-
- document.querySelector('.whatsapp-button').addEventListener('click', function() {
-    const socialIconsContainer = document.querySelector('.social-icons-container');
-    socialIconsContainer.classList.toggle('show');
+  var uluru = { lat: 37.227837, lng: -95.700513 };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: uluru
   });
-  
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
+document.querySelector('.whatsapp-button').addEventListener('click', function () {
+  const socialIconsContainer = document.querySelector('.social-icons-container');
+  socialIconsContainer.classList.toggle('show');
+});
+
 // carrusel
 
 function openModal(imageIndex) {
-    const modal = document.getElementById("myModal");
-    const modalImage = document.getElementById("modalImage");
-    const modalText = document.getElementById("modalText");
+  const modal = document.getElementById("myModal");
+  const modalImage = document.getElementById("modalImage");
+  const modalText = document.getElementById("modalText");
 
-    // Configura la imagen y el texto según la imagen del carrusel que se haya clicado
-    switch(imageIndex) {
-        case 1:
-            modalImage.src = "imagen1.jpg";
-            modalText.innerText = "Descripción para la Imagen 1";
-            break;
-        case 2:
-            modalImage.src = "imagen2.jpg";
-            modalText.innerText = "Descripción para la Imagen 2";
-            break;
-        case 3:
-            modalImage.src = "imagen3.jpg";
-            modalText.innerText = "Descripción para la Imagen 3";
-            break;
-    }
+  // Configura la imagen y el texto según la imagen del carrusel que se haya clicado
+  switch (imageIndex) {
+    case 1:
+      modalImage.src = "imagen1.jpg";
+      modalText.innerText = "Descripción para la Imagen 1";
+      break;
+    case 2:
+      modalImage.src = "imagen2.jpg";
+      modalText.innerText = "Descripción para la Imagen 2";
+      break;
+    case 3:
+      modalImage.src = "imagen3.jpg";
+      modalText.innerText = "Descripción para la Imagen 3";
+      break;
+  }
 
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 
 function closeModal() {
-    const modal = document.getElementById("myModal");
-    modal.style.display = "none";
+  const modal = document.getElementById("myModal");
+  modal.style.display = "none";
 }
 
 function openModal(imageIndex) {
-    const modal = document.getElementById("myModal");
-    const modalImage = document.getElementById("modalImage");
-    const modalText = document.getElementById("modalText");
+  const modal = document.getElementById("myModal");
+  const modalImage = document.getElementById("modalImage");
+  const modalText = document.getElementById("modalText");
 
-    switch(imageIndex) {
-        case 1:
-            modalImage.src = "assets/imgs/Header2.jpeg";
-            modalText.innerText = "Descripción para la Imagen 1";
-            break;
-        case 2:
-            modalImage.src = "assets/imgs/Header1.jpeg";
-            modalText.innerText = "Descripción para la Imagen 2";
-            break;
-        case 3:
-            modalImage.src = "assets/imgs/Header3.jpeg";
-            modalText.innerText = "Descripción para la Imagen 3";
-            break;
-    }
+  switch (imageIndex) {
+    case 1:
+      modalImage.src = "assets/imgs/Header2.jpeg";
+      modalText.innerText = "Descripción para la Imagen 1";
+      break;
+    case 2:
+      modalImage.src = "assets/imgs/Header1.jpeg";
+      modalText.innerText = "Descripción para la Imagen 2";
+      break;
+    case 3:
+      modalImage.src = "assets/imgs/Header3.jpeg";
+      modalText.innerText = "Descripción para la Imagen 3";
+      break;
+  }
 
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 
 function closeModal() {
-    const modal = document.getElementById("myModal");
-    modal.style.display = "none";
+  const modal = document.getElementById("myModal");
+  modal.style.display = "none";
 }
 
 
@@ -121,7 +121,7 @@ document.addEventListener('DOMContentLoaded', function () {
     dropdown.addEventListener('click', function (event) {
       // Evita el cierre inmediato al hacer clic dentro del menú
       event.stopPropagation();
-      
+
       // Cierra otros dropdowns abiertos
       dropdowns.forEach(function (otherDropdown) {
         if (otherDropdown !== dropdown) {
@@ -162,10 +162,20 @@ function toggleMenu() {
   const socialMenu = document.getElementById('socialMenu');
   const whatsappIcon = document.getElementById('whatsappIcon');
   socialMenu.classList.toggle('open');
-  
+
   if (socialMenu.classList.contains('open')) {
-      whatsappIcon.src = 'assets/imgs/Redes/icons8-circulado-x-50.png'; // X icon
+    whatsappIcon.src = 'assets/imgs/Redes/icons8-circulado-x-50.png'; // X icon
   } else {
-      whatsappIcon.src = 'assets/imgs/Redes/whatsapp_b.png'; // WhatsApp icon
+    whatsappIcon.src = 'assets/imgs/Redes/whatsapp_b.png'; // WhatsApp icon
   }
 }
+
+
+document.querySelectorAll('.logo').forEach(logo => {
+  logo.addEventListener('mouseover', function () {
+    this.style.backgroundImage = `url(${this.getAttribute('data-hover')})`;
+  });
+  logo.addEventListener('mouseout', function () {
+    this.style.backgroundImage = '';
+  });
+});
